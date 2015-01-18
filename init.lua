eb16113bb5e5f460c06a1a0184ae7bd6fac91b78
MoHorst <mo.horst@gmail.com>
2015-01-18 21:00:47 +0100

    start

---
+++ init.lua
@@ -0,0 +1,14 @@
+--GPIO
+GPIO2=4
+GPIO4=1
+GPIO5=2
+GPIO13=7
+GPIO14=5
+--GPIO
+
+id=0  -- need this to identify (software) IC2 bus?
+sda=3 -- connect to pin GPIO0
+scl=4 -- connect to pin GPIO2
+
+
+dofile("search_wifi.lua")
